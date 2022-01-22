f39a05fe4388cf2524cf3e811b392da8d9498b78
LucasBernard3 <bernard.lucas67@gmail.com>
2022-01-22 17:15:10 +0100

    travail weekend crepe

---
+++ camera/src/finger_counter_v2.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+# Import the necessary libraries
+import rospy # Python library for ROS
+from sensor_msgs.msg import Image # Image is the message type
+from cv_bridge import CvBridge # Package to convert between ROS and OpenCV Images
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+
+detector = HandDetector(detectionCon=0.5, maxHands=1)
+
+def hand_main(data):
+
+    br = CvBridge()
+    img = br.imgmsg_to_cv2(data)
+    
+    # Find the hand and its landmarks
+    hands, img = detector.findHands(img)
+    if hands:
+        hand1 = hands[0]
+        lmList1 = hand1["lmList"]  # List of 21 Landmark points
+        bbox1 = hand1["bbox"]  # Bounding box info x,y,w,h
+        centerPoint1 = hand1['center']  # center of the hand cx,cy
+        handType1 = hand1["type"]  # Handtype Left or Right
+
+        fingers1 = detector.fingersUp(hand1)
+        totalFingers = fingers1.count(1)
+        cv2.putText(img, f'Fingers:{totalFingers}', (bbox1[0] + 200, bbox1[1] - 30),
+                    cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2)
+    
+    # Display
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
+
+
+
+def ros_main():
+ 
+  # Tells rospy the name of the node.
+  # Anonymous = True makes sure the node has a unique name. Random
+  # numbers are added to the end of the name. 
+  rospy.init_node('video_sub_py', anonymous=True)
+   
+  # Node is subscribing to the video_frames topic
+  rospy.Subscriber('video_frames', Image, hand_main)
+ 
+  # spin() simply keeps python from exiting until this node is stopped
+  rospy.spin()
+ 
+  # Close down the video stream when done
+  cv2.destroyAllWindows()
+  
+if __name__ == '__main__':
+  ros_main()
--- camera/src/webcam_tout_con.py
+++
@@ -1,9 +0,0 @@
-import cv2 # OpenCV library
-cap = cv2.VideoCapture(0)
-
-while True :
-
-  ret, frame = cap.read()
-  if ret == True:
-    cv2.imshow('frame',frame)
-
