72ff0d796b00daf10a3c75bc057ab7b3d18aec4a
LucasBernard3 <bernard.lucas67@gmail.com>
2022-01-23 13:26:02 +0100

    Update finger_counter_visu.py

--- camera/src/finger_counter_visu.py
+++ camera/src/finger_counter_visu.py
@@ -3,19 +3,49 @@
 # Import the necessary libraries
 import rospy # Python library for ROS
 import cv2
-from sensor_msgs.msg import Image # Image is the message type
-from cv_bridge import CvBridge # Package to convert between ROS and OpenCV Images
+from std_msgs.msg import Int16 # Image is the message type
 from cvzone.HandTrackingModule import HandDetector
 
-detector = HandDetector()
+def automatic_brightness_and_contrast(image, clip_hist_percent=1):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    
+    # Calculate grayscale histogram
+    hist = cv2.calcHist([gray],[0],None,[256],[0,256])
+    hist_size = len(hist)
+    
+    # Calculate cumulative distribution from the histogram
+    accumulator = []
+    accumulator.append(float(hist[0]))
+    for index in range(1, hist_size):
+        accumulator.append(accumulator[index -1] + float(hist[index]))
+    
+    # Locate points to clip
+    maximum = accumulator[-1]
+    clip_hist_percent *= (maximum/100.0)
+    clip_hist_percent /= 2.0
+    
+    # Locate left cut
+    minimum_gray = 0
+    while accumulator[minimum_gray] < clip_hist_percent:
+        minimum_gray += 1
+    
+    # Locate right cut
+    maximum_gray = hist_size -1
+    while accumulator[maximum_gray] >= (maximum - clip_hist_percent):
+        maximum_gray -= 1
+    
+    # Calculate alpha and beta values
+    alpha = 255 / (maximum_gray - minimum_gray)
+    beta = -minimum_gray * alpha
 
-def hand_main(data):
+    auto_result = cv2.convertScaleAbs(image, alpha=alpha, beta=beta)
+    return (auto_result, alpha, beta)
 
-    br = CvBridge()
-    img = br.imgmsg_to_cv2(data)
+def hand_main(detector,img):
     
     # Find the hand and its landmarks
     hands, img = detector.findHands(img)
+    
     if hands:
         hand1 = hands[0]
         lmList1 = hand1["lmList"]  # List of 21 Landmark points
@@ -44,19 +74,35 @@ def hand_main(data):
 
 
 def ros_main():
- 
-  # Tells rospy the name of the node.
-  # Anonymous = True makes sure the node has a unique name. Random
-  # numbers are added to the end of the name. 
-  rospy.init_node('video_sub_py', anonymous=True)
-   
-  # Node is subscribing to the video_frames topic
-  rospy.Subscriber('video_frames', Image, hand_main)
- 
-  # spin() simply keeps python from exiting until this node is stopped
-  rospy.spin()
+
+  rospy.init_node('finger_counter_sw_py', anonymous=True)
+  
+  #initialise the hand detector
+  detector = HandDetector()
+
+  #initialise the camera
+  cap = cv2.VideoCapture(0)
+  
+  rate = rospy.Rate(10)
+  
+  while not rospy.is_shutdown():
+     
+      ret, frame = cap.read()
+         
+      if ret == True:
+      
+        auto_result, alpha, beta = automatic_brightness_and_contrast(frame)
+      
+        hand_main(detector,auto_result)
+        
+        # Print debugging information to the terminal
+        rospy.loginfo('publishing the number of fingers and the state of the thumb')
+             
+      # Sleep just enough to maintain the desired rate
+      rate.sleep()
  
   # Close down the video stream when done
+  cap.release()
   cv2.destroyAllWindows()
   
 if __name__ == '__main__':
